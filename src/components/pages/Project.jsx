78e3cc8ff1e8f9eb0134f6d4037efaec70c0675f
Gabriel Guarini <gabrielrguarini@gmail.com>
2021-12-14 13:10:57 -0300

    Adicionando formulário de serviços
    Adicionando formulário de serviços e custos aos projetos

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,16 +1,21 @@
+import { parse, v4 as uuidv4 } from "uuid";
 import styles from "./Project.module.css";
+
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
 import ProjectForm from "../projects/ProjectForm";
 import Message from "../layout/Message";
+import ServiceForm from "../service/ServiceForm";
+
 function Project() {
     const { id } = useParams();
-    //Testando Git
+
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
-
+    const [showServiceForm, setShowServiceForm] = useState(false);
     const [message, setMessage] = useState();
     const [type, setType] = useState();
 
@@ -29,6 +34,7 @@ function Project() {
     }, [id]);
 
     function editPost(project) {
+        setMessage("");
         //budget validation
         if (project.budget < project.cost) {
             setMessage("O orçamento não pode ser menor que o custo do projeto");
@@ -52,8 +58,40 @@ function Project() {
             .catch((err) => console.log(err));
     }
 
+    function createService(project) {
+        const lastService = project.services[project.services.length - 1];
+        lastService.id = uuidv4();
+        const lastServiceCost = lastService.cost;
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+        if (newCost > parseFloat(project.budget)) {
+            setMessage("Orçamento ultrapassado, verifique o valor do serviço");
+            setType("error");
+            project.services.pop();
+            return false;
+        }
+        project.cost = newCost;
+        console.log("Project antes do fetch", project);
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                //exibir os serviços
+                console.log("No fetch", data);
+            })
+            .catch((err) => console.log(err));
+    }
+
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm);
+    }
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm);
     }
 
     return (
@@ -95,6 +133,30 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button
+                                className={styles.btn}
+                                onClick={toggleServiceForm}
+                            >
+                                {!showServiceForm
+                                    ? "Adicionar serviço"
+                                    : "Fechar"}
+                            </button>
+                        </div>
+                        <div className={styles.datails_info}>
+                            {showServiceForm && (
+                                <ServiceForm
+                                    handleSubmit={createService}
+                                    btnText="Adicionar serviço"
+                                    projectData={project}
+                                />
+                            )}
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Serviços asd</p>
+                        </Container>
                     </Container>
                 </div>
             ) : (
